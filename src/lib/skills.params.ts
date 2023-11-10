6d7d2e1967d1b3538355f02239d9b201e27be3c7
Linko David <linkodavid.8@gmail.com>
2023-11-10 20:48:35 +0200

    Update skills.params.ts

--- src/lib/skills.params.ts
+++ src/lib/skills.params.ts
@@ -15,6 +15,24 @@ const MY_SKILLS = [
 			'I possess a comprehensive Lua proficiency, encompassing both the traditional Lua and its Roblox variant, Luau. With approximately three years of hands-on experience, I demonstrate adeptness in utilizing both versions effectively. This expertise underscores my ability to navigate diverse Lua development scenarios, ensuring a nuanced and skilled approach. My background serves as a testament to a thorough understanding and practical application of Lua, enhancing my capacity to contribute meaningfully to varied projects.',
 		logo: Assets.Lua,
 		name: 'Lua'
+	}),
+
+	s({
+		slug: 'ui',
+		color: 'gray',
+		description:
+			'I boast a year of dedicated experience in Roblox user interface design, witnessing notable progress in my endeavors. Proficient in industry-standard software such as Adobe Photoshop, Paint.NET, and Figma, I employ these tools to meticulously craft high-quality user interfaces and experiences. This commitment to utilizing advanced design platforms underscores my dedication to delivering impactful and visually compelling solutions within the dynamic realm of Roblox.',
+		logo: Assets.Lua,
+		name: 'UI / UX'
+	}),
+
+	s({
+		slug: 'translation',
+		color: 'gray',
+		description:
+			'Drawing upon my robust history on the Roblox platform as a Romanian translator, I have collaborated with various agencies and independently, establishing myself as a premier professional. Renowned for delivering unparalleled quality, competitive pricing, and efficient turnaround times, I am recognized as one of the foremost Romanian translators. My portfolio includes collaboration on esteemed games such as Doomspire Brickbattle, Vehicle Simulator, Vehicle Tycoon, Dealership Tycoon, Horrific Housing, among others, underscoring my diverse expertise in the gaming domain.',
+		logo: Assets.Lua,
+		name: 'Translation'
 	})
 ];
 
@@ -22,3 +40,4 @@ export default MY_SKILLS;
 
 export const getSkills = (...slugs: Array<string>): Array<Skill> =>
 	MY_SKILLS.filter((it) => slugs.includes(it.slug));
+
