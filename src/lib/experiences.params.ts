c6229a852cf3ccb17d42572a772777c63f15355e
Linko David <linkodavid.8@gmail.com>
2023-11-10 22:50:59 +0200

    Update experiences.params.ts

--- src/lib/experiences.params.ts
+++ src/lib/experiences.params.ts
@@ -14,10 +14,26 @@ const MY_EXPERIENCES: Array<Experience> = [
 		skills: getSkills('lua'),
 		name: 'Lead Programmer',
 		color: '#ffffff',
+		links: ['https://www.roblox.com/groups/4603811/WaCe-Studios#!/about'],
+		logo: Assets.Wace,
+		shortDescription: ''
+	},
+
+	{
+		slug: 'game-development-contractor',
+		company: 'Jamies Restaurant Franchise',
+		description: 'Hired as a contractor to re-write some of the old codebase along with making new systems.',
+		contract: ContractType.Freelance,
+		type: 'Game Development',
+		location: 'Remote',
+		period: { from: new Date(2021, 1, 1), to: new Date(2021, 2, 1) },
+		skills: getSkills('lua', 'ui'),
+		name: 'Contractor',
+		color: '#ffffff',
 		links: [],
 		logo: Assets.Wace,
 		shortDescription: ''
-	}
+	},
 ];
 
 export default MY_EXPERIENCES;
