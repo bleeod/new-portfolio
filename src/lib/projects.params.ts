dc45288b7a7bd3c20896785d518a79422e980106
Linko David <linkodavid.8@gmail.com>
2023-12-22 12:26:03 +0200

    Update projects.params.ts

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -20,8 +20,13 @@ const MY_PROJECTS: Array<Project> = [
 		type: '',
 		screenshots: [
 			{
-				label: 'System in-action',
+				label: 'ON-SITE',
 				src: Assets.GunSystem_GIF
+			},
+
+			{
+				label: 'URL',
+				src: "https://media.discordapp.net/attachments/831547738940112927/1187702167142944839/GunSystem_GIF.gif?ex=6597d8d7&is=658563d7&hm=5ae813d1ea01cd4878135ebfd8de96fef60b6f0272687c93546799f4c08f454a&="
 			}
 		]
 	},
@@ -43,8 +48,13 @@ const MY_PROJECTS: Array<Project> = [
 		type: '',
 		screenshots: [
 			{
-				label: 'System in-action',
+				label: 'ON-SITE',
 				src: Assets.SandboxTycoon_GIF
+			},
+
+			{
+				label: 'URL',
+				src: "https://media.discordapp.net/attachments/831547738940112927/1187702186344456192/SandboxTycoon_GIF.gif?ex=6597d8db&is=658563db&hm=6f5f96dcd5b781f30f94ebf59598ed970b9220dc39d185f5cb5bd1c9827ce34b&=&width=1264&height=676"
 			}
 		]
 	}
